67c81e41894fbb26c255efe2101ce340fdbfe4c9
James Denk <jndenk26@gmail.com>
2023-04-09 15:33:48 -0500

    built boilerplate HTML, JS, and CSS

---
+++ Assets/js/Script.js
@@ -0,0 +1,45 @@
+var containerQuestionEl = document.getElementById("question-container");
+
+
+
+
+
+
+var questions = [
+{
+    q: 'Inside which HTML element do we put the JavaScript?',
+    a: '1. <script>',
+    choices: [{choice: '1. <script>'}, {choice: '2. <scripting>'}, {choice: '3. <js>'}, {choice: '4. <Javascript>'}]
+},
+{
+    q: 'Where is the correct place to insert a JavaScript?',
+    a: '2. Both the <head> section and the <body> section are correct',
+    choices: [{choice: '1. The <head> section'}, {choice: '2. Both the <head> section and the <body> section are correct'}, {choice: '3. The <body> section'}]
+},
+{
+    q: 'The external JavaScript file must contain the <script> tag.',
+    a: '2. False',
+    choices: [{choice: '1. True'}, {choice: '2. False'}]
+},
+{
+    q: 'Which operator is used to assign a value to a variable?',
+    a: '1. =',
+    choices: [{choice: '1. ='}, {choice: '2. *'}, {choice: '3. -', choice: '4. X'}]
+},
+{
+    q: 'Is JavaScript case-sensitive?',
+    a: '1. Yes',
+    choices: [{choice: '1. Yes'}, {choice: '2. No'}]
+},
+{
+    q: 'How do you declare a JavaScript variable?',
+    a: '1. var carName;',
+    choices: [{choice: '1. var carName;'}, {choice: '2. variable carName;'}, {choice: '3. v carName;'}]
+},
+{
+    q: 'What will the following code return: Boolean(10 > 9)',
+    a: '2. true',
+    choices: [{choice: '1. NaN'}, {choice: '2. true'}, {choice: '3. false'}]
+},
+];
+
